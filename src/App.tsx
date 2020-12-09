c049dc7fb05303f9863eaf2a7d8caee672a1faad
Faktor 10 <oreillyross@gmail.com>
2020-12-09 05:32:04 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      Create a input with autocomplete and hits an google location api with lazy loading for locations, once selected 
+      outputs it below and then uses the location to get the weather.
+      and also displays it below.
+      later can add descriptions pulled from database using graphqll
+      and apollo from a node api running graphql yoga etc and postgrssql
+      This would be my hobby project.
+    </div>
+  );
+}
