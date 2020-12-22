c8e2468305ffd240daa6aa41e62930eb82a4937b
Ross O'Reilly <oreillyross@gmail.com>
2020-12-22 10:31:25 +0100

    start again

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import * as React from "react";
 import "./styles.css";
-import mapboxgl from "mapbox-gl";
+import {  } from "react-query";
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+const App = props => ( 
 
-export default class App extends React.Component {
-  
-    constructor(props) {
-    super(props);
-    this.state = {
-    lng: 5,
-    lat: 34,
-    zoom: 2
-    };
-    }
+  <div>My App</div> 
 
-  componentDidMount() {
-      const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-      });
-      }
+)
 
-  render() {
-    
-    return (
-      <>
-      rr
-      
-      <div className='mapContainer' ref={el => this.mapContainer = el}></div>
-      </>
-    )
-  }
-  
-   
-}
+export { App }
