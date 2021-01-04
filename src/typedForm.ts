9330607d86a0a221bdae9d4e0942b6b5682d188f
Ross O'Reilly <oreillyross@gmail.com>
2021-01-04 19:27:45 +0100

    contact form

---
+++ src/typedForm.ts
@@ -0,0 +1,19 @@
+interface Form<T> {
+  values: T;
+}
+
+interface contact {
+  name: string;
+  email: string;
+  approved: boolean;
+}
+
+const contactForm: Form<contact> = {
+  values: {
+    name: "Bob",
+    email: "bob@home.com",
+    approved: true,
+  },
+};
+
+export { contactForm };
