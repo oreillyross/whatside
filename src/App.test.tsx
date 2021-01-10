4e6eceb39d92d7f05cfed29a6e47c4f2cf35c305
Ross O'Reilly <oreillyross@gmail.com>
2021-01-10 14:56:20 +0100

    fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,10 @@
-import { timeStamp } from "console";
 import * as React from "react";
 
 test('Number of items is above 2', () => {
     expect(1).toBeLessThanOrEqual(1)
 }) 
+
+
+const data2 = ['React Native', 'React']
+
 
